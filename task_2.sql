9e981e4242d7204f9e05b2121bdb3048f2a9f102
aymenmuktar <aymennursefa70@gmail.com>
2025-08-06 02:47:44 -0700

    kiju

--- task_2.sql
+++ task_2.sql
@@ -28,7 +28,7 @@ CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details (
@@ -36,7 +36,7 @@ CREATE TABLE Order_Details (
     order_id INT NOT NULL,
     book_id INT NOT NULL,
     quantity INT NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
 
